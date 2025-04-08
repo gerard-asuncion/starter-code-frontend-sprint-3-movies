a5825940c2b9bf3f2c4d8487bd44defecf99e1ab
GerardAsuncion <asunciongerard90@gmail.com>
2025-04-08 20:33:44 +0200

    feat: exercises 2 & 3 done

--- src/films.js
+++ src/films.js
@@ -2,7 +2,7 @@ import movies from './data.js';
 
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result = array.map(movie => movie.director)
+  const result = array.map(movie => movie.director)
   console.log("EXERCICE 1 ->", result);
   return result;
 }
@@ -11,13 +11,24 @@ getAllDirectors(movies)
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  const result = array.filter(movie => movie.director == director)
+  console.log("EXERCICE 2 ->", result);
+  return result;
 }
+
+getMoviesFromDirector(movies, "Quentin Tarantino")
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  const movies = array.filter(movie => movie.director == director);
+  const sum = movies.reduce((acc, cur) => acc + cur.score, 0);
+  const average = sum / movies.length
+  let result = average.toFixed(2);
+  console.log("EXERCICE 3 ->", result);
+  return result;
 }
+
+moviesAverageOfDirector(movies, "Sergio Leone")
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
